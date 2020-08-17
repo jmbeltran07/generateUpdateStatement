325014a0cb8ab11f04de60f4336f4eee68512c05
Jose Manuel Beltran Loreto <jm.beltran.loreto@gmail.com>
2020-08-17 13:38:49 -0500

    update README and comment code

--- generateUpdateStatement.js
+++ generateUpdateStatement.js
@@ -4,14 +4,18 @@ const { Mutation } = require('./typeValidator')
 const generateUpdateStatement = (document, mutation) => {
     let output = {}
     let outputLine = {}
+
+    // verify the input agains valid Mutation structure
     try {
         var validMutation = Mutation(mutation)
     } catch (e){
         output = { "invalid mutation": true }
         return output
     }
+    // map against the valid Mutation to process each specific operation
     _.map(validMutation, (operations, subdocument) => {
         _.map(operations, (operation) => {
+            // destructure the operation into ints potential values
             const {
                 _id: postId = null,
                 value = null,
@@ -21,30 +25,46 @@ const generateUpdateStatement = (document, mutation) => {
                     _delete: mentionDelete = false,
                     text = null }
                 ] = [{}] } = operation
-            let postIndex = _.findIndex(_.get(document, 'posts'), { "_id": postId })
-            let mentionIndex = _.findIndex(_.get(document, `posts.${postIndex}.mentions`), { "_id": mentionId })
-            // There are 6 cases: $update, $add, $remove; each for posts and mentions
-            // Check for update posts
+
+            // calculate index against the provided document
+            const postIndex = _.findIndex(_.get(document, 'posts'), { "_id": postId })
+            const mentionIndex = _.findIndex(_.get(document, `posts.${postIndex}.mentions`), { "_id": mentionId })
+
             // if an id was provided, but index can't be found, then is an invalid operation
             if ((postId && postIndex === -1) || (mentionId && mentionIndex === -1)) {
                 outputLine = { "invalid operation": true }
             } else {
+                // There are 6 cases: $update, $add, $remove; each for posts and mentions
+                // Check for update posts
                 if (postId && value && !mentionId && !text && !postDelete && !mentionDelete) {
                     outputLine = _.set(outputLine, '$update', { [`${subdocument}.${postIndex}.value`]: value })
-                } else if (postId && !value && mentionId && text && !postDelete && !mentionDelete) {
+                }
+                // Check for update mentions
+                 else if (postId && !value && mentionId && text && !postDelete && !mentionDelete) {
                     outputLine = _.set(outputLine, '$update', { [`${subdocument}.${postIndex}.mentions.${mentionIndex}.text`]: text })
-                } else if (!postId && value && !mentionId && !text && !postDelete && !mentionDelete) {
+                } 
+                // Check for add post
+                else if (!postId && value && !mentionId && !text && !postDelete && !mentionDelete) {
                     outputLine = _.set(outputLine, "$add", { [`${subdocument}`]: [{ "value": value }] })
-                } else if (postId && !value && !mentionId && text && !postDelete && !mentionDelete) {
+                } 
+                // Check for add mention
+                else if (postId && !value && !mentionId && text && !postDelete && !mentionDelete) {
                     outputLine = _.set(outputLine, "$add", { [`${subdocument}.${postIndex}.mentions`]: { 'text': text } })
-                } else if (postId && !value && !mentionId && !text && postDelete && !mentionDelete) {
+                } 
+                // Check for remove post
+                else if (postId && !value && !mentionId && !text && postDelete && !mentionDelete) {
                     outputLine = _.set(outputLine, '$remove', { [`posts.${postIndex}`]: postDelete })
-                } else if (postId && !value && mentionId && !text && !postDelete && mentionDelete) {
+                } 
+                // Check for remove mention
+                else if (postId && !value && mentionId && !text && !postDelete && mentionDelete) {
                     outputLine = _.set(outputLine, '$remove', { [`${subdocument}.${postIndex}.mentions.${mentionIndex}`]: mentionDelete })
-                } else {
+                } 
+                // Else, we have an invalid operation
+                else {
                     outputLine = { "invalid operation": true }
                 }
             }
+            // merge the operation line into the output object
             if (outputLine) {
                 output = _.merge(output, outputLine)
             }
